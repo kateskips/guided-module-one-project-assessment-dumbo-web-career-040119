447472e976e5e72b3064d93ee748e21bb525e95c
Kate Lois Follett <katefollett>
2019-04-15 15:45:40 -0400

    changed to only active records

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,40 +1,4 @@
-class Restaurant
-    attr_reader :name, :user
-    attr_accessor :review
-
-  @@all = []
-
-  def initialize(name)
-    @name = name
-    @@all << self
-  end
-
-  def self.all
-    @@all
-  end
-
-  #returns an array of all reviews for that restaurant
-  #helper
-  def reviews
-    Review.all.select do |reviews|
-      reviews.restaurant == self
-    end
-  end
-
-  #Returns a **unique** list of all users who have reviewed a particular restaurant.
-  #First, get a list of all the reviews, then get all the users from those reviews
-  def users
-    reviews = Review.all.map {|review| review.user}
-    reviews.uniq
-  end
-  
-
-  #returns the average star rating for a restaurant based on its reviews
-  def average_star_rating
-  end
-
-  #given a string of restaurant name, returns the first restaurant that matches
-  def self.find_by_name(name)
-    @@all.find{|restaurant| restaurant.name == name}
-  end
+class Restaurant < ActiveRecord::Base
+  has_many :reviews
+  has_many :users, through: :reviews
 end
--- lib/review.rb
+++ lib/review.rb
@@ -1,18 +1,4 @@
-class Review
-    attr_reader :user
-    attr_accessor :restaurant, :content, :rating
-
-    @@all = []
-
-    def initialize(restaurant, content, rating, user)
-        @restaurant = restaurant
-        @content = content
-        @rating = rating
-        @user = user
-        @@all << self
-    end
-
-    def self.all
-        @@all
-    end
+class Review < ActiveRecord::Base
+    belongs_to :user
+    belongs_to :restaurant
 end
