b8ce1b5156bcbd86ec38dc87da820ef44a1cd73e
Kate Lois Follett <katefollett>
2019-04-16 10:57:00 -0400

    Added a CLI

--- bin/run.rb
+++ bin/run.rb
@@ -1,30 +1,10 @@
 require_relative '../config/environment'
 require 'pry'
 
-#restaurants
-lostacos = Restaurant.new("Los Tacos")
-spice = Restaurant.new("Spice Symphony")
 
-#user
-adam = User.new("Adam", "Staiger")
-matt = User.new("Matt", "Sanders")
-amy = User.new("Amy", "Chen")
-lizzy = User.new("Lizzy", "Oh")
-sam = User.new("Sam", "Kim")
-
-#reviews
-
-satisfied = Review.new(spice, "Satisfied", 5, adam)
-nogood = Review.new(lostacos, "No Good", 1, matt)
-pricy = Review.new(spice, "Too Pricy", 2, amy)
-love = Review.new(spice, "A Sense of Spice", 4, lizzy)
-sucks = Review.new(lostacos, "Worse than Chipotle", 1, adam)
-delish = Review.new(spice, "AMAZING!", 5, amy)
-yum = Review.new(lostacos, "Yum Yum", 3, lizzy)
+cli = CommandLineInterface.new
+cli.greet
 
 
-
-
-binding.pry
 
 0
